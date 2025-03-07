241bfba8d94360f9e16af655024246bff2f5f7e1
danikagiron <girondanika@gmail.com>
2025-03-07 13:06:46 +0800

    Simple Lightbox Gallery
    Added a simple lightbox gallery for the photos.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
+/* Hamburger Menu*/
 hamburger = document.querySelector(".hamburger");
 hamburger.onclick = function () {
     navBar = document.querySelector(".nav-bar");
     navBar.classList.toggle("active");
 }
+
+
+/* Lightbox for Gallery */
+const lightbox = document.createElement('div')
+lightbox.id = 'lightbox'
+document.body.appendChild(lightbox)
+
+const images = document.querySelectorAll('img.galleryimage')
+images.forEach(image =>{
+    image.addEventListener('click', e=>{
+        lightbox.classList.add('active')
+        const img = document.createElement('img')
+        img.src = image.src
+        while(lightbox.firstChild){
+            lightbox.removeChild(lightbox.firstChild)
+        }
+        lightbox.appendChild(img)
+    })
+})
+
+lightbox.addEventListener('click', e=>{
+    if (e.target !== e.currentTarget) return
+    lightbox.classList.remove('active')
+})
